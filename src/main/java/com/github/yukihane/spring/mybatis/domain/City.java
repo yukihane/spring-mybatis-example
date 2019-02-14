03111366d5102853d7791bd204f763bbd46778c6
yukihane <yukihane.feather@gmail.com>
2019-02-14 23:48:31 +0900

    Cityエンティティ作成

---
+++ src/main/java/com/github/yukihane/spring/mybatis/domain/City.java
@@ -0,0 +1,16 @@
+package com.github.yukihane.spring.mybatis.domain;
+
+import lombok.Data;
+
+@Data
+public class City {
+
+    private Long id;
+
+    private String name;
+
+    private String state;
+
+    private String country;
+
+}

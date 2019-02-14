0159a398c5b18486276396468350526ecd1506d3
yukihane <yukihane.feather@gmail.com>
2019-02-14 23:48:18 +0900

    デフォルトだとパッケージが長いので移動

--- src/main/java/com/github/yukihane/spring/mybatis/SpringMybatisExampleApplication.java
+++ src/main/java/com/github/yukihane/spring/mybatis/SpringMybatisExampleApplication.java
@@ -1,4 +1,4 @@
-package com.github.yukihane.spring.mybatis.springmybatisexample;
+package com.github.yukihane.spring.mybatis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

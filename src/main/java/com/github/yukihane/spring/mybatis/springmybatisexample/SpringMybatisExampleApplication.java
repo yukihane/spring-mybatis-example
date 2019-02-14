fb2bdbd74605a3ce05ba3bf72c97b52149c62f55
yukihane <yukihane.feather@gmail.com>
2019-02-14 23:28:42 +0900

    auto-generated files

---
+++ src/main/java/com/github/yukihane/spring/mybatis/springmybatisexample/SpringMybatisExampleApplication.java
@@ -0,0 +1,14 @@
+package com.github.yukihane.spring.mybatis.springmybatisexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMybatisExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMybatisExampleApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/github/yukihane/spring/mybatis/springmybatisexample/SpringMybatisExampleApplicationTests.java
@@ -0,0 +1,17 @@
+package com.github.yukihane.spring.mybatis.springmybatisexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringMybatisExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+

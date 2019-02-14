2566b3a1325c1ccb66f667fa8c2cf4d50665ec0a
yukihane <yukihane.feather@gmail.com>
2019-02-15 00:19:44 +0900

    アノテーション方式からXML方式に変換

--- src/main/java/com/github/yukihane/spring/mybatis/mapper/CityMapper.java
+++ src/main/java/com/github/yukihane/spring/mybatis/mapper/CityMapper.java
@@ -3,12 +3,10 @@ package com.github.yukihane.spring.mybatis.mapper;
 import com.github.yukihane.spring.mybatis.domain.City;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface CityMapper {
 
-    @Select("select * from city where state = #{state}")
     City findByState(@Param("state") String state);
 
 }

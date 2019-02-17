b470754de721a8033a2251bcda760d22ac6e9480
yukihane <yukihane.feather@gmail.com>
2019-02-17 23:39:10 +0900

    整形

--- src/main/java/com/github/yukihane/spring/mybatis/SpringMybatisExampleApplication.java
+++ src/main/java/com/github/yukihane/spring/mybatis/SpringMybatisExampleApplication.java
@@ -6,9 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringMybatisExampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringMybatisExampleApplication.class, args);
-	}
+    public static void main(final String[] args) {
+        SpringApplication.run(SpringMybatisExampleApplication.class, args);
+    }
 
 }
-
